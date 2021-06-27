efd84097a1143c7b53506cef1ccfa8cf83944c25
杨康 <851161583@qq.com>
2021-06-27 11:45:44 +0800

    再来一次

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
+    dest: '.vuepress/dist',
+    locales: { '/': { lang: 'zh-CN', }, },
     themeConfig: {
         nav: require('./nav'),
         sidebar: require('./nav'),
