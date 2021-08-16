42ff9c60d8e0ea16815a178f1b7bfb01ac1474a8
杨康 <851161583@qq.com>
2021-08-16 22:55:21 +0800

    update

--- docs/code/java/sidebar.js
+++ docs/code/java/sidebar.js
@@ -1,11 +1,21 @@
 module.exports = [
     {
-        title: '写在开头',   // 必要的
+        title: '前言',   // 必要的
         path: '/code/java/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: false, // 可选的,是否允许折叠 默认值是 true,
     },
     {
-        title: '菜单2',
+        title: '线程',
+        children: [ /* ... */ ],
+        initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+    },
+    {
+        title: 'jvm',
+        children: [ /* ... */ ],
+        initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+    },
+    {
+        title: '***',
         children: [ /* ... */ ],
         initialOpenGroupIndex: -1 // 可选的, 默认值是 0
     }
